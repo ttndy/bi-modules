af6753cee03cf8aa33b64b2bf54c6d06f3004c87
Mitchell Danford <mitchell.danford@ttigroupna.com>
2024-04-24 21:13:37 +0000

    Update SSL certificate handling in config_vd_dsn.py

--- bi_modules/config_vd_dsn.py
+++ bi_modules/config_vd_dsn.py
@@ -1,30 +1,34 @@
 import pyodbc
 from prefect.blocks.system import Secret, String
-import os
-import tempfile
-
-secret_block_uid = Secret.load("vd-uid")
-secret_block_pwd = Secret.load("vd-pwd")
-secret_block_cacerts = Secret.load("dc-cacerts")
-
-cacerts = secret_block_cacerts.get()
-uid = secret_block_uid.get()
-pwd = secret_block_pwd.get()
-host = String.load("dataconnect-host").value
-
-port="20502"
-ssl="1"
-dsn="VENDORDRILL"
-
+from prefect_azure import AzureBlobStorageCredentials, AzureBlobStorageContainer
 
 
 
 def connect():
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.pem') as temp_cert_file:
-        temp_cert_file.write(cacerts.encode())
-        temp_cert_path = temp_cert_file.name
+        secret_block_uid = Secret.load("vd-uid")
+        secret_block_pwd = Secret.load("vd-pwd")
 
-    try:
+        uid = secret_block_uid.get()
+        pwd = secret_block_pwd.get()
+
+        host=String.load("dataconnect-host").value
+
+        port="20502"
+        ssl="1"
+        azure_credentials_block = AzureBlobStorageCredentials.load('azure-blob-storage-credentials-ryobi-01',validate=False)
+        block = AzureBlobStorageContainer(
+            container_name="certs",
+            credentials=azure_credentials_block,
+        )
+
+        file = "cacerts.pem"
+
+        with open('/opt/' + file , "wb") as f:
+            block.download_object_to_file_object(
+                from_path='dataconnect/' + file,
+                to_file_object=f
+            )
+
         at_scale_connection = pyodbc.connect(
         #                 Destination of your HDBC Driver  
                 "DRIVER=/opt/cloudera/hiveodbc/lib/64/libclouderahiveodbc64.so" +
@@ -36,22 +40,16 @@ def connect():
                 ";AllowSelfSignedServerCert=1" +
                 ";CAIssuedCertNamesMismatch=1" +
         #                 Destination of your cacerts.pem file
-                ";TrustedCerts=" + temp_cert_path +
+                ";TrustedCerts=" + '/opt/' + file +
         #                 Your Login Credentials
                 ";UID=" + uid +
                 ";PWD=" + pwd,autocommit=True)
         return at_scale_connection
-    
-    except Exception as e:
-        print("Connection failed: ", e)
-        return None
-    
-    finally:
-        os.unlink(temp_cert_path)
-
 
 conn_vd = connect()
 
 
+
 if __name__ == '__main__':
         connect()
+
